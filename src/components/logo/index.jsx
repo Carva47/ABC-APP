42e61755ab2ef5c10fd1118264d08eaf4ab2257b
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-04-09 15:52:22 +0100

    feat: Header

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Logo = () => {
+  return (
+    <Link to="/">
+      <h1 className="text-white text-4xl font-extrabold">ABC</h1>
+    </Link>
+  )
+}
