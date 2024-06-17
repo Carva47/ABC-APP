44f9d0de45edb3c6079734798654f94fd3886383
Nvuala Carvalho <nvualacanvula@gmail.com>
2024-06-17 13:53:42 +0100

    update resposive leyou userprofile

--- src/pages/ProfileUser/index.jsx
+++ src/pages/ProfileUser/index.jsx
@@ -1,27 +1,25 @@
+import React from 'react';
 import TimelineHeader from "../../components/TimelineHeader/TimelineHeader";
 import { PrincipalPage } from '../../layout/principalPage';
-import {Intro} from '../../components/Intro/intro';
+import { Intro } from '../../components/Intro/intro';
 import { CreatePost } from '../../components/CreatePost/CreatePost';
 import { MainPost } from "../../components/MainPost/MainPost";
 import { Posts } from "../../components/CreatePost/Posts";
 
-export function UserProfile(){
-
+export function UserProfile() {
   return (
-      <PrincipalPage>
+    <PrincipalPage>
       <TimelineHeader />
-
-          <div className=" grid grid-cols-12 pt-4 gap-4 z-0 pb-56">
-           <div className="col-span-4 col-start-1 row-start-1  space-y-4">
+      <div className="grid grid-cols-1 lg:grid-cols-12 pt-4 gap-4 z-0 pb-56">
+        <div className="col-span-1 lg:col-span-4 lg:col-start-1 space-y-4">
           <Intro />
-           </div>
-           <div className="flex-row row-start-1 col-span-12 col-start-5 space-y-4">
+        </div>
+        <div className="col-span-1 lg:col-span-8 lg:col-start-5 space-y-4">
           <CreatePost />
           <Posts />
-          <MainPost/>
-
+          <MainPost />
         </div>
-           </div>
-      </PrincipalPage>
-  )
+      </div>
+    </PrincipalPage>
+  );
 }
